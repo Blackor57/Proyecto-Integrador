310f37f0216e16e9c522f4c7efa795aedd2db530
Edu <ea.luisperez@gmail.com>
2024-10-30 00:19:17 -0500

    arreglo de las clases

--- src/main/java/com/example/proyectointegrador/controller/ProductoControlador.java
+++ src/main/java/com/example/proyectointegrador/controller/ProductoControlador.java
@@ -1,10 +1,12 @@
 package com.example.proyectointegrador.controller;
 
+import com.example.proyectointegrador.services.ProductoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@RequiredArgsConstructor
+@Controller
 public class ProductoControlador {
-
-    @Autowired
-    private ProductoService productoService;
-
+    private final ProductoService productoService;
 }
--- src/main/java/com/example/proyectointegrador/services/ProductoService.java
+++ src/main/java/com/example/proyectointegrador/services/ProductoService.java
@@ -4,14 +4,16 @@ import com.example.proyectointegrador.mapper.ProductoMapper;
 import com.example.proyectointegrador.model.dto.ProductoListaDTO;
 import com.example.proyectointegrador.model.entity.Producto;
 import com.example.proyectointegrador.repository.ProductoRepositorio;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
+@RequiredArgsConstructor
 public class ProductoService {
-
-    @Autowired
     private ProductoRepositorio productoRepositorio;
 
     public List<ProductoListaDTO> obtenerProductosPorCategoria(Long categoriaId) {
